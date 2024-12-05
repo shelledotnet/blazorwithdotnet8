e262f5e6fefca5f7b856ad9b0b4e4d98e0366c5b
shelle mohammed olatunji <shelledotnet@outlook.com>
2024-12-05 18:36:39 +0100

    .

---
+++ MyWebAPP/MyWebAPP/Program.cs
@@ -0,0 +1,7 @@
+var builder = WebApplication.CreateBuilder(args); //tis builder is kestrel web server
+var app = builder.Build(); // tis app  is te web application
+
+app.MapGet("/", () => "Hello World! welcome to minimal api");  //tis is te middlwear component
+//containin a routin and lamda expression (a call bark function)
+
+app.Run(); //tis run web application
---
+++ OnlineAccountOpeningService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FCMB.BVNMiddleWareService.WebApiApplication" Language="C#" %>
